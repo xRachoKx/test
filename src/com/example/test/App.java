1c4856cb2aab487b75d45abb5910d65a23c6911d
xRachoKx <lukmanovartem5@gmail.com>
2024-11-09 16:02:54 +0500

    while

--- src/com/example/test/App.java
+++ src/com/example/test/App.java
@@ -2,6 +2,8 @@ package com.example.test;
 
 public class App {
     public static void main(String[] args) {
-        System.out.println("Hello, world!");
+        while (true){
+            System.out.println(1);
+        }
     }
 }
